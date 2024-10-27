b2aae6e518cd8e618ffd98eea915671df93e760a
Ajay Kumar <theajaykumar29@gmail.com>
2024-10-27 21:22:03 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -26,14 +26,23 @@ function App() {
       <header className="App-header">
         <h1 className="app-title">CRUD Based Application</h1>
         <p className="app-subtitle">ProdigalAI Internship Assignment</p>
-        <p></p><a href="https://github.com/ajju-devs/CRUD-app-ProdigalAI-assignment" target="_blank" rel="noopener noreferrer" className="project-link">
+        <a href="https://github.com/ajju-devs/CRUD-app-ProdigalAI-assignment" 
+           target="_blank" 
+           rel="noopener noreferrer" 
+           className="project-link">
           For GitHub Repository Click Here
         </a>
       </header>
-      <RouterProvider router={route}></RouterProvider>
+      
+      <div className="main-content"> {/* Added a wrapper for the main content */}
+        <RouterProvider router={route}></RouterProvider>
+      </div>
+      
       <footer className="App-footer">
         <p>Built by: Ajay Kumar 10219011621 USAR AI-ML 4th year</p>
-        <p>Github: <a href="https://github.com/ajju-devs" target="_blank" rel="noopener noreferrer">https://github.com/ajju-devs</a></p>
+        <p>Github: <a href="https://github.com/ajju-devs" target="_blank" rel="noopener noreferrer">
+          https://github.com/ajju-devs
+        </a></p>
       </footer>
     </div>
   );
